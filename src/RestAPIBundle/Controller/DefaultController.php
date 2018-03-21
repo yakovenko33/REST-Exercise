25abc051e17e80ec07bcde5a0ff3d5b6c6ca0c8c
Vlad Yakovenko <botvot33@gmail.com>
2018-03-21 17:38:11 +0200

    add form

--- src/RestAPIBundle/Controller/DefaultController.php
+++ src/RestAPIBundle/Controller/DefaultController.php
@@ -6,13 +6,16 @@ use RestAPIBundle\Entity\Film;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use RestAPIBundle\Service\FilmParserService;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         /*$curl = curl_init();
         $url = 'http://www.omdbapi.com/';
@@ -35,13 +38,50 @@ class DefaultController extends Controller
         exit;*/
 
         $film = new Film();
+        $repository = $this->getDoctrine()->getRepository(Film::class);
 
-        $example = 'Blade Runner';
+
+
+        $form = $this->createFormBuilder($film)
+            ->add('title', TextType::class)
+            ->add('save', SubmitType::class, array('label' => 'Create Task'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $form = $form->getData();
+
+
+
+            $product = $repository->findOneBy(['title' => $film->getTitle()]);
+
+
+
+
+
+
+            if(is_null($product)){
+                $move = $this->get('rest_api');
+                $result = $move->searchMoves($film->getTitle());
+                $move->addMoves($film, $result);
+
+            }
+
+
+        exit;
+            return $this->render('RestAPIBundle:Default:index.html.twig', array(
+                'form' => $form->createView(),
+            ));
+        }
+
+
+
+        /*$example = 'Justice League';
         $repository = $this->getDoctrine()->getRepository(Film::class);
         $product = $repository->findOneBy(['title' => $example]);
 
-        /*print_r(is_null($product));
-        exit;*/
+
 
         if(is_null($product)){
             $move = $this->get('rest_api');
@@ -49,10 +89,7 @@ class DefaultController extends Controller
             $move->addMoves($film, $result);
         }
 
-        exit;
-
-
-
+        exit;*/
 
 
         /*foreach($result as $value){
@@ -63,6 +100,8 @@ class DefaultController extends Controller
 
 
 
-        return $this->render('RestAPIBundle:Default:index.html.twig', array());
+        return $this->render('RestAPIBundle:Default:index.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
